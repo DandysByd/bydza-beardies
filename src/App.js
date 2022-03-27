caafc85bae016ee5b2be0024daeded11a19d8054
DandysByd <bydzak@gmail.com>
2022-03-27 23:00:19 +0200

    init

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     
     return (   
     <>
-<Router>
+<Router basename='/bbeardies'>
         <nav id='navbar'>
                 <ul className='first-ul'>
                 <NavLink onClick={closeMenu} className='contact-link' exact activeClassName='is-active' to="/contact">Contact</NavLink>
