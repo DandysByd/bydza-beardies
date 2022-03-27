f808fa40f65dd944fa125b0f74a7d3969516cbad
DandysByd <bydzak@gmail.com>
2022-03-27 21:25:46 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './Styles/main_css.css';
 import './Styles/querries.css';
 
 ReactDOM.render(
-  <HashRouter>
+  <React.StrictMode>
     <App />
-  </HashRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 reportWebVitals();
