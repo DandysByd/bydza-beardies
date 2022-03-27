6223deba1d0a8bac33aeee02059a88f9f4393151
DandysByd <bydzak@gmail.com>
2022-03-27 22:40:37 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './Styles/main_css.css';
 import './Styles/querries.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 reportWebVitals();
