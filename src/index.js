599dd2c2f0c723071a6ea9587d885d27f8c74fd6
DandysByd <bydzak@gmail.com>
2022-03-28 11:31:22 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Breeders from './Sites/Contact'
 import reportWebVitals from './reportWebVitals';
 import './Styles/main_css.css';
 import './Styles/querries.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Breeders />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
