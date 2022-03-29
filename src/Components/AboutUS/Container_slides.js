968a81ddca75b5cf72d8be92ee20aeb2429a21e6
DandysByd <bydzak@gmail.com>
2022-03-29 19:32:05 +0200

    mobile adjustments

--- src/Components/AboutUS/Container_slides.js
+++ src/Components/AboutUS/Container_slides.js
@@ -18,7 +18,7 @@ function Container_slides() {
                 text='Currently we own 2 bearded dragons that are not for sale. They provide us both, very good company and opportunity to look at the world of domesticaded lizards in completely new angle via small beardies.'
                 image={require('../../images/Breeders/Chilli_brumation.jpg')}
                 alt='Chilli'
-                link='../../bydza-beardies/breeders'
+                link='/bydza-beardies/breeders'
             >
             </Fading_div>
             <Fading_div
@@ -28,7 +28,7 @@ function Container_slides() {
                 text='Everytime new beardies are born, we take photos of them and recognize genetics. Then we provide all of theese informations in this tab so our customers can clearly see, who are they buying.'
                 image={require('../../images/Breeders/babybeardeddragon.jpg')}
                 alt='Saab'
-                link='../../bydza-beardies/dragons-for-sale'
+                link='/bydza-beardies/dragons-for-sale'
             >
             </Fading_div>
             <Fading_div
@@ -38,7 +38,7 @@ function Container_slides() {
                 text="Let's take a look at ideal conditions which you, as a keeper, need to provide for your beardie to let him/her prosper. Here you can also read about the journey that your new bearded dragon has to walk through to get to his new enclousure in his new home."
                 image={require('../../images/Gallery/terariums.jpg')}
                 alt='Chilli'
-                link='../../bydza-beardies/care-guide-and-more'
+                link='/bydza-beardies/care-guide-and-more'
             >
             </Fading_div>
         </>
